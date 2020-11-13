f15f26e379ff67bc594bd628393ec1a5cc2e0c2b
yubi00 <ubrajkhadka@gmail.com>
2020-11-13 20:43:24 +1100

    new xlsx file added

--- app.js
+++ app.js
@@ -1,19 +1,22 @@
 // jshint esversion: 6
-var http = require('http');
-var emp = require('./worksheet');
-var fs = require('fs');
-var ejs = require('ejs');
+var http = require('http')
+var emp = require('./worksheet')
+var ejs = require('ejs')
 
-http.createServer((req, res) => {
-    var empdata = JSON.stringify(emp.getEmployee());
-    res.end(ejs.renderFile('index.ejs', {employee: JSON.parse(empdata), title: 'List of Employees'}, (err, data) => {
-        res.write(data);
-    }));
-    
-}).listen(3000, (err, result) => {
-    if(!err) {
-        console.log('The server started successfully');
+http
+  .createServer((req, res) => {
+    res.end(
+      ejs.renderFile(
+        'index.ejs',
+        { employee: emp.getEmployee(), title: 'List of Employees' },
+        (err, data) => {
+          res.write(data)
+        }
+      )
+    )
+  })
+  .listen(3000, (err, result) => {
+    if (!err) {
+      console.log('The server started successfully')
     }
-});
-
-
+  })
--- worksheet.js
+++ worksheet.js
@@ -1,17 +1,16 @@
-const xlsx = require('xlsx');
+const xlsx = require('xlsx')
 
-var getEmployee = function() {
-    const workbook = xlsx.readFile('./employeedata/Book.xlsx');
+var getEmployee = function () {
+  const workbook = xlsx.readFile('./employeedata/Book.xlsx')
 
-    //get the array of sheets in excel file
-    const sheet_name_list = workbook.SheetNames;
+  //get the array of sheets in excel file
+  const sheet_name_list = workbook.SheetNames
 
-    //get the worksheet that you want to work on
-    var ws = workbook.Sheets[sheet_name_list[0]];
+  //get the worksheet that you want to work on
+  var ws = workbook.Sheets[sheet_name_list[0]]
 
-    var employeedata = xlsx.utils.sheet_to_json(ws);
-    return employeedata;
-
+  var employeedata = xlsx.utils.sheet_to_json(ws)
+  return employeedata
 }
 
-module.exports.getEmployee = getEmployee;
+module.exports.getEmployee = getEmployee
